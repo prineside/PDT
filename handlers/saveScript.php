993d15cdf29d6f04b1a06d0c3a69c3f75c124348
Vadim Sachno <prineside@mail.ru>
2013-05-12 00:07:39 +0300

    Revert "Исправлена обработка данны�"
    This reverts commit 7b275afe45157ab96f2c98efde5ad59450b94708.

--- handlers/saveScript.php
+++ handlers/saveScript.php
@@ -1,7 +1,7 @@
 <?php
 	if($_POST['args']['script'] && $_POST['args']['content']){
 		$src = fopen(PDT_WORKING_DIR.'/scripts/'.$_POST['args']['script'].'.php', 'w');
-		fwrite($src, $_POST['args']['content']);
+		fwrite($src, stripslashes($_POST['args']['content']));
 		fclose($src);
 		echo json_encode(array('success' => 'true'));
 	}else{
